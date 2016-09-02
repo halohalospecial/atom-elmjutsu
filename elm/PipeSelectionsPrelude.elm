f81d2b0f1f4223a364b75bf444166d366192c25b
halohalospecial <halohalospecial@gmail.com>
2016-09-02 10:55:02 +0800

    Add Pipe Selections screenshots

--- elm/PipeSelectionsPrelude.elm
+++ elm/PipeSelectionsPrelude.elm
@@ -3,28 +3,15 @@ module PipeSelectionsPrelude exposing (..)
 import List as L
 import String as S
 import String exposing (..)
-import String.Extra exposing (..)
 
 
-enumerate =
-    List.indexedMap (\i a -> toString (i + 1) ++ ". " ++ a)
+enumerate : String -> List String
+enumerate text =
+    String.lines text
+        |> List.indexedMap (\i a -> toString (i + 1) ++ ". " ++ a)
 
 
-appendAll b =
-    List.map (\a -> a ++ b)
-
-
-prependAll b =
-    List.map (\a -> b ++ a)
-
-
-trimAll =
-    List.map String.trim
-
-
-toFloatList list =
-    List.map (String.toFloat >> Result.withDefault 0) list
-
-
-total list =
-    list ++ [ List.sum (toFloatList list) |> toString ]
+linesMap : (String -> String) -> String -> List String
+linesMap fn text =
+    String.lines text
+        |> List.map fn
